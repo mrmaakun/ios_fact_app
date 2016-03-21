981760429937b5143e6614a0cbabe3eff471dd2c
Mark Serrano <mark.serrano@gmail.com>
2016-03-22 00:53:22 +0900

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Mark Serrano on 3/22/16.
+//  Copyright © 2016 Mark Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FunFacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FunFacts
+//
+//  Created by Mark Serrano on 3/22/16.
+//  Copyright © 2016 Mark Serrano. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
